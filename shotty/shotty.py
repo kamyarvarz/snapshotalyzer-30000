c6840859d1ce15273604aa6efe8bce65f6a1ce82
Varzandeh <kamyar.varzandeh@pacificlife.com>
2019-05-09 15:38:05 -0700

    added code to stop servers

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -4,7 +4,11 @@ import click
 session = boto3.Session(profile_name='identity')
 ec2 = session.resource('ec2')
 
-@click.command()
+@click.group()
+def instances():
+        """Commands for instances"""
+
+@instances.command('list')
 @click.option('--purpose', default=None, help="Only instances for this purpose (tag purpose:<name>)")
 
 def list_instances(purpose):
@@ -29,7 +33,24 @@ def list_instances(purpose):
 
     return
 
+@instances.command('stop')
+@click.option('--purpose', default=None, help="Only instances for this purpose (tag purpose:<name>)")
+def stop_instances(purpose):
+    "Stop EC2 instances"
+    instances = []
+
+    if purpose:
+        filters = [{'Name':'tag:purpose', 'Values': [purpose]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    for i in instances:
+        print("Stopping {0}...".format(i.id))
+        i.stop()
+    return
+
 if __name__ == '__main__':
     
-    list_instances()
+    instances()
         
