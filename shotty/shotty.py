7122e15a798124562236dfae7545eb9a2f287df1
Varzandeh <kamyar.varzandeh@pacificlife.com>
2019-05-09 15:11:11 -0700

    filtering by tag using --purpose optional argument

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -5,15 +5,27 @@ session = boto3.Session(profile_name='identity')
 ec2 = session.resource('ec2')
 
 @click.command()
-def list_instances():
+@click.option('--purpose', default=None, help="Only instances for this purpose (tag purpose:<name>)")
+
+def list_instances(purpose):
     "List EC2 instances"
-    for i in ec2.instances.all():
+    instances = []
+
+    if purpose:
+        filters = [{'Name':'tag:purpose', 'Values': [purpose]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    for i in instances:
+        tags = {t['Key']: t['Value'] for t in i.tags or []}
         print(', '.join((
                 i.id,
                 i.instance_type,
                 i.placement['AvailabilityZone'],
                 i.state['Name'],
-                i.public_dns_name)))
+                i.public_dns_name,
+                tags.get('purpose', '<no tag>'))))
 
     return
 
