f5df571416997ce389389e059cb0585a6a38a580
Varzandeh <kamyar.varzandeh@pacificlife.com>
2019-05-13 17:30:43 -0700

    version 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='snapshotalyzer-30000',
-    version='0.1',
+    version='0.2',
     author='Kamyar Varzandeh',
     author_email='kamyar.varzandeh@pacificlife.com',
     description="Snapshotalyzer 30000 is a tool to manage AWS EC2 snapshots",
